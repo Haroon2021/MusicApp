878ff7dd89e937c2f00b3bf3a115232ce2a4ddad
haroon <haroon@Dures-MacBook-Air.local>
2022-05-15 17:07:25 +0100

    Second commit

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,10 +1,17 @@
+import axios from 'axios';
 import './App.css';
 import { Home } from './components/Home';
+import { SearchComponent } from './components/Search';
+import {  MusicPlayer  } from './components/MusicPlayer';
+import { options } from './utils/api';
 
 function App() {
   return (
-    <div className="App">
+    <div className="home">
       <Home />
+      <SearchComponent />
+      <MusicPlayer />
+      <options />
     </div>
   );
 }
--- music-app/src/components/Home.js
+++ music-app/src/components/Home.js
@@ -1,6 +1,5 @@
 // import React, { useState } from "react";
 
-
 export const Home = () => {
     return (
         <div>
