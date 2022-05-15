46de0db6a41542e48aef6f7d0dd61b4a0ec5da55
haroon <haroon@Dures-MacBook-Air.local>
2022-05-15 17:58:03 +0100

    Testing api

--- music-app/src/utils/api.js
+++ music-app/src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-
 const options = {
   method: 'GET',
-  url: 'https://spotify23.p.rapidapi.com/tracks/',
-  params: {ids: '4WNcduiCmDNfmTEz7JvmLv'},
+  url: 'https://spotify23.p.rapidapi.com/artists/',
+  params: {ids: '2w9zwq3AktTeYYMuhMjju8'},
   headers: {
     'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
     'X-RapidAPI-Key': '268d1365d5mshd8fed733c9404b8p1898c0jsn11b717f9a09a'
@@ -16,3 +15,4 @@ axios.request(options).then(function (response) {
 }).catch(function (error) {
 	console.error(error);
 });
+
